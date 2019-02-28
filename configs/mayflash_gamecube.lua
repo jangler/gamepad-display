049301ca49e3cf536e2cff4d042227aa84433ac9
Brandon Mulcahy <brandon@lightcones.net>
2019-02-28 00:03:43 -0500

    Make gamecube NES/GB layout more compact and abstract. also larger

--- configs/mayflash_gamecube.lua
+++ configs/mayflash_gamecube.lua
@@ -2,31 +2,31 @@ local draw = require "draw"
 
 return {
 	name = "mayflash limited MAYFLASH GameCube Controller Adapter",
-	drawBackground = function(js) draw.polygon(1, 2, 2, 8) end,
-	width = 11,
-	height = 5.5,
-	inputs = { -- gamecube layout
+	drawBackground = function(js) draw.polygon(1, 1, 3, 8) end,
+	width = 9.5,
+	height = 5,
+	inputs = { -- gamecube layout for game boy / nes
 		stick = {
 			isActive = function(js) return true end,
 			draw = function(js)
-				draw.circle(1.25 + js:getAxis(1), 2.25 + js:getAxis(2), 1.5)
+				draw.circle(1.5 + js:getAxis(1), 1.5 + js:getAxis(2), 2)
 			end,
 		},
 		start = {
 			isActive = function(js) return js:isDown(10) end,
-			draw = function() draw.circle(5.125, 3, 0.75) end,
+			draw = function() draw.circle(4.5, 1, 1) end,
 		},
 		a = {
 			isActive = function(js) return js:isDown(2) end,
-			draw = function() draw.circle(8.25, 2.25, 1.5) end,
+			draw = function() draw.circle(6.5, 2, 2) end,
 		},
 		b = {
 			isActive = function(js) return js:isDown(3) end,
-			draw = function() draw.circle(7, 3.5, 1) end,
+			draw = function() draw.circle(5, 3, 1) end,
 		},
 		z = {
 			isActive = function(js) return js:isDown(8) end,
-			draw = function() draw.bar(8, 1, 2, 0.5) end,
+			draw = function() draw.bar(6.5, 1, 2, 0.5) end,
 		},
 	},
 }
